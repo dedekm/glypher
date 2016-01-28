a44709d1754c2fdf0de88cb899c383571ad57f07
Martin Dedek <martin.dedek@sinfin.cz>
2016-01-28 11:30:22 +0100

    + alphabet.js, more letters

--- glypher.js
+++ glypher.js
@@ -27,59 +27,6 @@ Generator.prototype.generate = function() {
   }
 };
 
-function Alphabet(xheight) {
-  xheight = xheight || 5;
-
-  this.glyphs = {
-    i: [
-      [0, 10 - xheight],
-      [0, 10]
-    ],
-    o: [
-      [0, 10],
-      [0, 10 - xheight],
-      [10, 10 - xheight],
-      [10, 10],
-      [0, 10]
-    ],
-    A: [
-      [0, 10],
-      [5, 0],
-      [10, 10]
-    ],
-    I: [
-      [0, 0],
-      [0, 10]
-    ],
-    P: [
-      [0, 10],
-      [0, 0],
-      [10, 0],
-      [10, 10 - xheight],
-      [0, 10 - xheight]
-    ],
-    O: [
-      [0, 10],
-      [0, 0],
-      [10, 0],
-      [10, 10],
-      [0, 10]
-    ],
-    S: [
-      [0, 10],
-      [10, 10],
-      [10, 10 - xheight],
-      [0, 10 - xheight],
-      [0, 0],
-      [10, 0],
-    ]
-  };
-}
-
-Alphabet.prototype.availableGlyphs = function() {
-  return Object.keys(this.glyphs);
-};
-
 function Glyph(name, weight, contrast, proportion) {
   this.name = name;
   this.weight = weight;
