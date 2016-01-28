a7573052e7271d4b5bae102d18a22276d623fc93
Martin Dedek <martin.dedek@sinfin.cz>
2016-01-28 14:25:18 +0100

    * proportions wip

--- glypher.js
+++ glypher.js
@@ -28,9 +28,9 @@ Generator.prototype.generate = function() {
 };
 
 Generator.prototype.getGlyph = function(name) {
-  if(this.glyphs[name]){
+  if (this.glyphs[name]) {
     return this.glyphs[name];
-  }else {
+  } else {
     console.log('glyph not defined');
     return false;
   }
@@ -54,12 +54,14 @@ Generator.prototype.generateGlyph = function(name, points) {
   var box = new Point(glyph.weight, glyph.contrast);
 
   for (var i = 0; i < points.length - 1; i++) {
-
-
     var path = new Path();
 
-    var p1 = new Point(points[i]).multiply([glyph.size / glyph.proportion, glyph.size]);
-    var p2 = new Point(points[i + 1]).multiply([glyph.size / glyph.proportion, glyph.size]);
+    var p1 = new Point(points[i])
+      .multiply([glyph.size / glyph.proportion, glyph.size - (glyph.contrast * 2 / glyph.size)])
+      .add(glyph.weight, glyph.size * 0.5);
+    var p2 = new Point(points[i + 1])
+      .multiply([glyph.size / glyph.proportion, glyph.size - (glyph.contrast * 2 / glyph.size)])
+      .add(glyph.weight, glyph.size * 0.5);
 
     var vector = p2.subtract(p1);
     var x = sign(vector.x);
@@ -89,7 +91,6 @@ Generator.prototype.generateGlyph = function(name, points) {
     if (p2.x + glyph.weight > glyph.width)
       glyph.width = p2.x + glyph.weight;
   }
-
   glyph.path = glyph.mergeSegments(segments);
 
   return glyph;
@@ -107,9 +108,20 @@ Glyph.prototype.mergeSegments = function(segments) {
 
 //DEBUG
 Glyph.prototype.draw = function(x, y) {
+  //debugging
+  new Path.Circle({
+    center: [x,y],
+    radius: 3,
+    strokeColor: 'blue'
+  });
+  new Path.Circle({
+    center: [x + this.width,y],
+    radius: 3,
+    strokeColor: 'blue'
+  });
 
   var path = this.path.clone();
-  path.position = [x + path.position.x, y + path.position.y];
+  path.position = [x + path.position.x, y - path.position.y];
   path.strokeColor = 'black';
 };
 
